20c6adb825abfba511c879687b15ee209ce5ec7c
Nikita <nikitaefron06@gmail.com>
2024-07-09 03:17:32 +0200

    Update server.js

--- src/server.js
+++ src/server.js
@@ -3,50 +3,36 @@
 import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
-import { env } from './utils/env.js';
+import { setupServer } from './utils/env.js';
+import { errorHandler, notFoundHandler } from './middlewares/errorHandlers.js';
 import contactsRouter from './routers/contacts.js';
-import { ENV_VARS } from './constants/index.js';
-import createError from 'http-errors'; // імпорт пакету http-errors
 
-const setupServer = () => {
-  const PORT = env(ENV_VARS.PORT, '3000');
-  const app = express();
+const app = express();
+const PORT = setupServer();
 
-  app.use(
-    pino({
-      transport: {
-        target: 'pino-pretty',
-      },
-    }),
-  );
-  app.use(cors());
+app.use(
+  pino({
+    transport: {
+      target: 'pino-pretty',
+    },
+  }),
+);
+app.use(cors());
+app.use(express.json());
 
-  // Використовуємо роутер контактів для маршруту /contacts
-  app.use('/contacts', contactsRouter);
+// Підключення роутера для контактів
+app.use(contactsRouter);
 
-  // Middleware для обробки помилок 404 (маршрути не знайдені)
-  const notFoundHandler = (req, res, next) => {
-    next(createError(404, 'Route not found'));
-  };
+// Middleware для обробки помилок 404 (неіснуючі маршрути)
+app.use(notFoundHandler);
 
-  app.use(notFoundHandler);
+// Middleware для обробки всіх інших помилок
+app.use(errorHandler);
 
-  // Middleware для обробки всіх інших помилок
-  const errorHandler = (err, req, res, next) => {
-    const { status = 500, message = 'Something went wrong', data } = err;
-    res.status(status).json({ status, message, data });
-  };
-
-  app.use(errorHandler);
-
-  // Запуск сервера
-  app.listen(PORT, (error) => {
-    if (error) {
-      console.log('Server crushed. error: ', error);
-      process.exit(1);
-    }
-    console.log('Server is running on port', PORT);
-  });
-};
-
-export default setupServer;
+app.listen(PORT, (error) => {
+  if (error) {
+    console.error('Server crushed. Error:', error);
+    process.exit(1);
+  }
+  console.log('Server is running on port', PORT);
+});
