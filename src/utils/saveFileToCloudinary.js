bde67f7ca07fb763964c590323b6af3a38f153ef
Nikita <nikitaefron06@gmail.com>
2024-07-29 03:12:54 +0200

    Sunnary (required)

--- src/utils/saveFileToCloudinary.js
+++ src/utils/saveFileToCloudinary.js
@@ -2,20 +2,17 @@ import cloudinary from 'cloudinary';
 import fs from 'node:fs/promises';
 import { env } from './env.js';
 
+import { CLOUDINARY } from '../constants/index.js';
+
 cloudinary.v2.config({
   secure: true,
-  cloud_name: env(CLOUDINARY.CLOUD_NAME),
-  api_key: env(CLOUDINARY.API_KEY),
-  api_secret: env(CLOUDINARY.API_SECRET),
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 export const saveFileToCloudinary = async (file) => {
-  try {
-    const response = await cloudinary.v2.uploader.upload(file.path);
-    await fs.unlink(file.path);
-    return response.secure_url;
-  } catch (error) {
-    console.error('Error uploading to Cloudinary:', error);
-    throw new Error('Cloudinary upload failed');
-  }
+  const response = await cloudinary.v2.uploader.upload(file.path);
+  await fs.unlink(file.path);
+  return response.secure_url;
 };
